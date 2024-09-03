5a30a7ca91f18329cb21ef422bc827ad678f3aa3
davidsqui <davidsqr6@gmail.com>
2024-09-02 22:41:16 -0500

    feat(module): implement customer service module

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer.customers (id, username, first_name, last_name) 
+VALUES ('f47b3b3b-3b3b-3b3b-3b3b-3b3b3b3b3b3b', 'user_1', 'John', 'Doe');
+
+INSERT INTO customer.customers (id, username, first_name, last_name)
+VALUES ('f47b3b3b-3b3b-3b3b-3b3b-3b3b3b3b3b3c', 'user_2', 'Alex', 'Black');
+
