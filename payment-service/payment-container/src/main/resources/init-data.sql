1909a8785ed855d77f6fd8d20ee94326006ea4d2
davidsqui <davidsqr6@gmail.com>
2024-09-03 00:00:51 -0500

    feat(module): implement payment container module

---
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO payment.credit_entry (id, customer_id, total_credit_amount)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b31', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b32', 500.00);
+INSERT INTO payment.credit_history (id, customer_id, amount, type)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b33', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b32', 100.00, 'CREDIT');
+INSERT INTO payment.credit_history (id, customer_id, amount, type)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b35', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b32', 600.00, 'CREDIT');
+INSERT INTO payment.credit_history (id, customer_id, amount, type)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b37', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b32', 200.00, 'CREDIT');
+
+INSERT INTO payment.credit_entry (id, customer_id, total_credit_amount)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b39', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b33', 100.00);
+INSERT INTO payment.credit_history (id, customer_id, amount, type)
+VALUES ('f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b41', 'f7b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b33', 100.00, 'CREDIT');
